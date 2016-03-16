b42646e2ed1257e6459fd762ba73a31b915cd12a
tristantrim <tristantrim@gmail.com>
2016-03-15 21:49:01 -0300

    A bit cleaned up. Shows dates now. Foo, bar.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 
 
-An application for looking at your lobsters written with kivy.
+An application for looking at your lobsters, written with kivy.
 
+
+Todo:
+ - change the LobsterListItems to accept all the info from the feed, and display it formatted more proper like.
+ - have the width of the text depend on the width of the screen.
+ - have a web browser or something open when links are clicked.
+
+If you start working on a todo item, let me know first. If you have any questions, ask ask ask.
 
--- main.py
+++ main.py
@@ -7,31 +7,42 @@ import rethinkdb
 from kivy.properties import ListProperty
 from kivy.adapters.listadapter import ListAdapter
 
-class BetterLIL(ListItemLabel):
-    def __init__(self,*args,**kwargs):
-        if not 'halign' in kwargs:
-            kwargs['halign']='left'
-        kwargs['text_size']=(500,20)
-        super(BetterLIL,self).__init__(*args,**kwargs)
+class LobsterListItem(ListItemLabel):
+   #def __init__(self,*args,**kwargs):
+   #    if not 'halign' in kwargs:
+   #        kwargs['halign']='left'
+   #    kwargs['text_size']=(500,20)
+   #    super(BetterLIL,self).__init__(*args,**kwargs)
+    def on_touch_up(self,touch):
+        if self.collide_point(*touch.pos):
+            print("A silly monkey says: {}".format(self.text))
 
 class LobsterList(ListView):
     def __init__(self, *args, **kwargs):
         #connect to db and get data
         conn = rethinkdb.connect(host='localhost',port=28018)
-        posts = [post['title'] for post in \
-                    rethinkdb.db('lobsters').table('feeditems').run(conn) \
-                        ]
+        db = rethinkdb.db('lobsters')
+        table = db.table('feeditems')
+        posts = table.order_by(rethinkdb.desc('updated')).run(conn)
+        post_titles = [post['title'] for post in posts]
+#       posts = [post['title'] for post in \
+#                   rethinkdb.db('lobsters').table('feeditems').get_all().order_by(rethinkdb.desc('date')).run(conn) \
+#                       ]
         #put data into scrollable list
         def args_converter(row_index,text):
-            if len(text) > 70:
-                text = text[:67]+"..."
-            return {'text': text,
+            #text = text.rstrip()
+            if 'updated_parsed' in text.keys():
+                updated = '{:0>2} {:0>2} {:0>2} {:0>2}:{:0>2}: '.format(*text['updated_parsed'][:5])
+            else:
+                updated = "???? ?? ?? ??:??: "
+            return {'text': updated+": "+text['title'],
                 'size_hint_y': None,
-                'height': 25,
-                'text_size':(600,20)}
-        list_adapter = ListAdapter(data=posts,
+                'height': 50,#len(text)/3+20,
+                'valign':'middle',
+                'text_size':(650,50)}#len(text)/3+10)}
+        list_adapter = ListAdapter(data=posts,#_titles,
             args_converter=args_converter,
-            cls=ListItemLabel
+            cls=LobsterListItem
                 )
         super(LobsterList, self).__init__(
                 adapter=list_adapter)
