9a40ae84ba947120763d8799d562fef86c690a9d
tristantrim <tristantrim@gmail.com>
2016-03-18 08:10:51 -0300

    Lay out the basics of the LobsterListItem. It is pretty ok exept that I don't know how to make the text_size change based on the window resizing. Yet.

--- main.py
+++ main.py
@@ -2,20 +2,48 @@
 import kivy
 from kivy.app import App
 from kivy.uix.listview import ListView, ListItemLabel
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 import rethinkdb
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, StringProperty
 from kivy.adapters.listadapter import ListAdapter
+import webbrowser
 
-class LobsterListItem(ListItemLabel):
-   #def __init__(self,*args,**kwargs):
-   #    if not 'halign' in kwargs:
-   #        kwargs['halign']='left'
-   #    kwargs['text_size']=(500,20)
-   #    super(BetterLIL,self).__init__(*args,**kwargs)
+from kivy.clock import Clock
+
+class LobsterListItem(BoxLayout):
+    title = StringProperty()
+    link = StringProperty()
+    timestamp = StringProperty()
+    def __init__(self,*args,**kwargs):
+        self.title = kwargs['title'] if 'title' in kwargs else '???' 
+        self.link = kwargs['link'] if 'link' in kwargs else '' 
+        self.timestamp = kwargs['timestamp'] if 'timestamp' in kwargs else "???? ?? ?? ??:?? : " 
+        del kwargs['title']
+        del kwargs['link']
+        del kwargs['timestamp']
+        if 'orientation' in kwargs:
+            if kwargs['orientation'] == 'vertical':
+                print("fuck you, it's horizontal")
+        kwargs['orientation'] = 'horizontal'
+
+        title = Label(text=self.title,valign='top', size_hint=(.7, 1))
+        timestamp = Label(text=self.timestamp,valign='top',halign='right', size_hint=(.3, 1))
+        kwargs['height']=70
+        super(LobsterListItem,self).__init__(*args,**kwargs)
+        self.add_widget(timestamp)
+        self.add_widget(title)
+        Clock.schedule_interval(self.resetTextSizeTest, .1)# omg this is the worst solution, I can't believe I'm doing this even temporarily, ulg.
+    def resetTextSizeTest(self, wut):
+        self.children[0].text_size=self.children[0].size
+        self.children[1].text_size=self.children[1].size
+
+
     def on_touch_up(self,touch):
         if self.collide_point(*touch.pos):
-            print("A silly monkey says: {}".format(self.text))
+            webbrowser.get('firefox').open_new_tab(self.link)
+            print("A silly monkey says: {}".format(self.link))
 
 class LobsterList(ListView):
     def __init__(self, *args, **kwargs):
@@ -24,18 +52,19 @@ class LobsterList(ListView):
         db = rethinkdb.db('lobsters')
         table = db.table('feeditems')
         posts = table.order_by(rethinkdb.desc('updated')).run(conn)
-        post_titles = [post['title'] for post in posts]
         #convert data for presentation
         def args_converter(row_index,text):
             if 'updated_parsed' in text.keys():
-                updated = '{:0>2} {:0>2} {:0>2} {:0>2}:{:0>2}: '.format(*text['updated_parsed'][:5])
+                updated = '{:0>2} {:0>2} {:0>2} {:0>2}:{:0>2} : '.format(*text['updated_parsed'][:5])
             else:
                 updated = "???? ?? ?? ??:??: "
-            return {'text': updated+": "+text['title'],
-                'size_hint_y': None,
-                'height': 50,#len(text)/3+20,
-                'valign':'middle',
-                'text_size':(650,50)}#len(text)/3+10)}
+            return {'title': text['title'],
+                    'link':text['link'],
+                    'timestamp':updated,
+                    'size_hint_y': None,
+                    'height': 50,#len(text)/3+20,
+                    'valign':'middle',
+                    'text_size':(650,50)}#len(text)/3+10)}
         list_adapter = ListAdapter(data=posts,
             args_converter=args_converter,
             cls=LobsterListItem
